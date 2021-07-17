d8abada753f72da5aff24feba002de3d383eca7b
ShatterPlayer <shatterplayer@gmail.com>
2021-07-17 13:30:32 +0200

    Added allowed characters to regexes

--- utils/regexes.js
+++ utils/regexes.js
@@ -18,15 +18,15 @@ module.exports = {
   regexCorrectAnswer: new RegExp(`^[0-${answersAmount - 1}]$`),
   regexId: /^[0-9a-f]{24}$/,
   regexQuizTitle: new RegExp(
-    `^[a-z0-9żźćńółęąśŻŹĆĄŚĘŁÓŃ ]{${minQuizTitleChars},${maxQuizTitleChars}}$`,
+    `^[a-z0-9'żźćńółęąśŻŹĆĄŚĘŁÓŃ?!.,<>/\\[\\]\\)\\(;:"#$%^&*+-_ ]{${minQuizTitleChars},${maxQuizTitleChars}}$`,
     'i'
   ),
   regexQuestionText: new RegExp(
-    `^[a-z0-9'"żźćńółęąśŻŹĆĄŚĘŁÓŃ ]{${minQuestionChars},${maxQuestionChars}}$`,
+    `^[a-z0-9'żźćńółęąśŻŹĆĄŚĘŁÓŃ?!.,<>/\\[\\]\\)\\(;:"#$%^&*+-_ ]{${minQuestionChars},${maxQuestionChars}}$`,
     'i'
   ),
   regexQuestionAnswer: new RegExp(
-    `^[a-z0-9żźćńółęąśŻŹĆĄŚĘŁÓŃ ]{${minAnswerChars},${maxAnswerChars}}$`,
+    `^[a-z0-9'żźćńółęąśŻŹĆĄŚĘŁÓŃ?!.,<>/\\[\\]\\)\\(;:"#$%^&*+-_ ]{${minAnswerChars},${maxAnswerChars}}$`,
     'i'
   ),
   regexQuestionNumber: /^[0-9]+$/,
